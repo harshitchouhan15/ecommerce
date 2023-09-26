1fff2d68a2921c78d139d96cbca9ac0d2478ce8b
harshit-chouhan <harshitc315@gmail.com>
2023-09-27 03:02:04 +0530

    Fix path module error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
+const path = require('path')
 const orderRoute=require('./routes/order')
 const cartRoute=require('./routes/cart')
 const productRoute=require('./routes/product')
