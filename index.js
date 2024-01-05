808f85de0c992577467bc803a2f8fcd8cdf9beca
harshitchouhan15 <harshitc315@gmail.com>
2024-01-05 18:36:03 +0530

    added

--- index.js
+++ index.js
@@ -38,3 +38,4 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
+// tresyasgdkbdka
